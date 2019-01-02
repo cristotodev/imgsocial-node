ddb8a16460938d7d10497e2c2eac08ec619c1595
Cristo Estévez <cristom.estevez@gmail.com>
2019-01-02 16:00:48 +0000

    List all image from bd into home page

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,10 @@
+const { Image } = require('../models')
+
 const ctrl = {}
 
-ctrl.index = (req, res) => {
-    res.render('index');
+ctrl.index = async(req, res) => {
+    const images = await Image.find().sort({ timestamp: -1 });
+    res.render('index', { images });
 };
 
 module.exports = ctrl;
